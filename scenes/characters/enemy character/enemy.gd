ebd540deabd739f2de2def84061f31584780aadb
Viel <vielavernus@gmail.com>
2026-07-03 01:01:23 +0300

    create scenes for player, enemy and base characters and update for current state

--- scenes/characters/enemy character/enemy.gd
+++ scenes/characters/enemy character/enemy.gd
@@ -6,3 +6,8 @@ func _on_death():
 	anim_comp.play_anim("death")
 	movement_comp.move_to_target(GameState.get_death_zone())
 	collision.disabled = true
+
+
+func _ready() -> void:
+	super._ready()
+	movement_comp.move_to_target(GameState.player_character)
--- scripts/autoload/GameState.gd
+++ scripts/autoload/GameState.gd
@@ -5,7 +5,7 @@ var death_zone: Node2D
 
 func save_player(player: Soldier):
 	print(player.name, ": ", player.get_groups())
-	if player.is_in_group("player") and !player.is_in_group("enemy"):
+	if player is Soldier:
 		player_character = player
 
 func update_unit_stats():
